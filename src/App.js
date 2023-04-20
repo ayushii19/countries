5dfb5060b140673a99fecfdcd1ce96260e49c1ab
ayushii19 <singhayushi1907@gmail.com>
2023-04-20 12:53:54 +0530

    sbj

--- src/App.js
+++ src/App.js
@@ -58,11 +58,14 @@ class App extends React.Component {
   // this.setState({
   //   data:value2
   // });
+
 }
+
 
  
   render() {
     const data = this.state.data;
+    let language=[]
     return (
       <div className="maincontent">
         <div className="maincontent__heading">
@@ -80,47 +83,55 @@ class App extends React.Component {
               type="submit" className="maincontent__subjectarea__inputarea__button" onClick={(e)=>this.handleChange(e)}>SEARCH</button>
           </div>
         </div>
-        {data.map((item, itemIndex) => (
-          <div key={ itemIndex } className="maincontent__card">
-            <div className="maincontent__card__info">
-              <div className="maincontent__card__info__name">
-                {item.name.common} {item.flag}
-              </div>
-              <div className="maincontent__card__info__capital" >
-                Capital : {item.capital}
-              </div>
-              <div className="maincontent__card__info__population">
-                Population : {item.population}
-              </div>
-              {item.currencies && (
-                <div className="maincontent__card__info__currencies">
-                  Currencies : {Object.keys(item.currencies).map((key, i) => (<React.Fragment  key={ key }><span>{key}({item.currencies[key].name})</span><br/>
-                   <span>Symbol : {item.currencies[key].symbol}</span></React.Fragment>
-                    
-                  ))}
+        {data.map((item, itemIndex) => {
+
+          const language = [];
+
+          // create language string from language object...
+          item.languages && Object.keys(item.languages).map((key, i) => {
+            language.push( `${ item.languages[key] } (${ key })` );
+          } );
+            const languageString = language.join( ',' );
+          // { languageString }
+          return (
+            <div key={ itemIndex } className="maincontent__card">
+              <div className="maincontent__card__info">
+                <div className="maincontent__card__info__name">
+                  {item.name.common} {item.flag}
                 </div>
-              )}
-              <div className="maincontent__card__info__flag">
-                 {item.flags.alt && <span>Info : {item.flags.alt}</span>}
-                 {!item.flags.alt && <span>Info : "No Information Found"</span>}
-              </div>
-              {item.languages && (
-              <div className="maincontent__card__info__languages">
-                  Languages : {Object.keys(item.languages).map((key, i) => (<React.Fragment  key={ key }><span>{key}</span><span>({(item.languages[key])}) </span>
-                   </React.Fragment>
-                    
-                  ))}
+                <div className="maincontent__card__info__capital" >
+                  Capital : {item.capital}
                 </div>
-              )}
+                <div className="maincontent__card__info__population">
+                  Population : {item.population}
+                </div>
+                {item.currencies && (
+                  <div className="maincontent__card__info__currencies">
+                    Currencies : {Object.keys(item.currencies).map((key, i) => (<React.Fragment  key={ key }><span>{key}({item.currencies[key].name})</span><br/>
+                     <span>Symbol : {item.currencies[key].symbol}</span></React.Fragment>
+                      
+                    ))}
+                  </div>
+                )}
+                <div className="maincontent__card__info__flag">
+                   {item.flags.alt && <span>Info : {item.flags.alt}</span>}
+                   {!item.flags.alt && <span>Info : "No Information Found"</span>}
+                </div>
+                
+                <div className="maincontent__card__info__languages">
+                    Languages :{languageString}
+                  </div>
+                
+              </div>
+              <div className="maincontent__card__image">
+                <img
+                  className="maincontent__card__image__image1"
+                  src={item.flags.png}
+                />
+              </div>
             </div>
-            <div className="maincontent__card__image">
-              <img
-                className="maincontent__card__image__image1"
-                src={item.flags.png}
-              />
-            </div>
-          </div>
-        ))}
+          )
+        })}
       </div>
     );
   }
